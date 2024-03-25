29e5d525813c28a7dbcaa2524a7a18692432ddd5
Bruna <53488109+brunaboeger@users.noreply.github.com>
2024-03-25 12:12:09 -0300

    feat: removed storybook content

---
+++ src/components/Fields/TextField/TextField.stories.js
@@ -0,0 +1,15 @@
+import TextField from './TextField.vue'
+
+export default {
+  title: 'Components/Fields/TextField',
+  component: TextField,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {}
+}
+export const Default = {
+  args: {
+    label: 'Full Name',
+    placeholder: 'Type something...'
+  }
+}
